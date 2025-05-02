f4b015a2f474625d09da6f6cdfca82df9e4780cd
root <takuma.itoga@rooter.co.jp>
2025-05-02 20:12:32 +0900

    検索ボックスで左右すると戻る進むバグがあったため、一旦ページング機能をなくす

--- src/content.js
+++ src/content.js
@@ -2,8 +2,8 @@
   this.focusIndex = 0;
   this.focusItem = function(index) {
     var t = this.links();
-    if (index < 0) { this.clickPrev(); }
-    else if (index >= t.length) { this.clickNext(); };
+    if (index < 0) { return }
+    else if (index >= t.length) { return };
     this.focusIndex = index;
     var n = t[this.focusIndex];
     return n.focus(), n
@@ -15,18 +15,11 @@
      }))
   };
 
-  this.clickLink = function(ele){ ele && ele.click(); }
-  this.clickNext = function(){ this.clickLink(document.querySelector('#pnnext')); };
-  this.clickPrev = function(){ this.clickLink(document.querySelector('#pnprev')); };
   this.actByKeydown = function(event){
       if (event.key === 'ArrowDown') {
         let _, focusedEle = this.focusItem(this.focusIndex+1);
       } else if (event.key === 'ArrowUp') {
         let _, focusedEle = this.focusItem(this.focusIndex-1);
-      } else if (event.key === 'ArrowRight') {
-        this.clickNext();
-      } else if (event.key === 'ArrowLeft') {
-        this.clickPrev();
       }
   };
   document.addEventListener('keydown', this.actByKeydown.bind(this));
